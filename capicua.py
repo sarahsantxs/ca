f75daf5ad45dd560ea58e4c896cc757a81bbfe4e
Sara Santos <143342414+sarahsantxs@users.noreply.github.com>
2024-02-27 17:15:24 -0300

    Add files via upload

---
+++ capicua.py
@@ -0,0 +1,8 @@
+print ("Determine se seu número é um tipo de capicua:")
+milhar = int (input ("Informe o valor da unidade de milhar:"))
+centena = int (input("Informe o valor da unidade de centena:"))
+dezena = int (input("Informe o valor da unidade de dezena:"))
+unidade = int (input("Informe o valor da unidade:"))
+
+if milhar==unidade and centena==dezena : print("O valor digitado é uma capicua!!")
+else : print ("O valor digitado não é uma capicua, sinto muito!")
